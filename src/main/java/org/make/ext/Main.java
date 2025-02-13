4f18f2aa65695982a592455a233c5f2e33966385
ff <vorjohnl@gmail.com>
2025-02-13 17:32:19 +0800

    init commit

---
+++ src/main/java/org/make/ext/Main.java
@@ -0,0 +1,31 @@
+package org.make.ext;
+
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.channels.Channels.newReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final List<String> warnings = Lists.newArrayList();
+        try (FileChannel ch = FileChannel.open(Paths.get("config.xml"))) {
+            ConfigurationParser parser = new ConfigurationParser(warnings);
+            Configuration cfg = parser.parseConfiguration(newReader(ch, StandardCharsets.UTF_8));
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator g = new MyBatisGenerator(cfg, callback, warnings);
+            g.generate(null, ImmutableSet.of("thor"));
+        } catch (Exception err) {
+            //
+        }
+    }
+}
