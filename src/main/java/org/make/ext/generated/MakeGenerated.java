536c0e7d5ac6cd6d8ea1170895d753a4a6188125
ff <vorjohnl@gmail.com>
2025-02-15 23:10:57 +0800

    feat: rename

--- src/main/java/org/make/ext/generated/MakeGenerated.java
+++ src/main/java/org/make/ext/generated/MakeGenerated.java
@@ -1,12 +1,18 @@
 package org.make.ext.generated;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.mybatis.generator.api.GeneratedJavaFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Date;
+import java.time.Instant;
+
 public abstract class MakeGenerated {
 
-    protected Logger log = LoggerFactory.getLogger(this.getClass());
+    public static String GENERATED = "@Generated(value = {\" ext-thor \"} , date = \" " + DateFormatUtils.format(Date.from(Instant.now()), "yyyy-MM-dd HH:mm:SS") + " \")";
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public abstract GeneratedJavaFile makeGeneratedFile();
 
