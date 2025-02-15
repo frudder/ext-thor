b6c34218ba012b36baa8971ad08312bdbeb646ae
ff <vorjohnl@gmail.com>
2025-02-15 23:10:45 +0800

    feat: lombok

--- src/main/java/org/make/ext/DefaultJavaField.java
+++ src/main/java/org/make/ext/DefaultJavaField.java
@@ -7,12 +7,15 @@ import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.mybatis.generator.api.dom.java.FullyQualifiedJavaType.getDateInstance;
 import static org.mybatis.generator.api.dom.java.JavaVisibility.PRIVATE;
 import static org.mybatis.generator.api.dom.java.JavaVisibility.PUBLIC;
+import static org.mybatis.generator.internal.util.JavaBeansUtil.getGetterMethodName;
 import static org.mybatis.generator.internal.util.JavaBeansUtil.getSetterMethodName;
 
 public enum DefaultJavaField {
@@ -24,17 +27,18 @@ public enum DefaultJavaField {
             value.setFinal(true);
             value.setStatic(true);
             value.setVisibility(PRIVATE);
-            value.setInitializationString("1L");
+            value.setInitializationString(UUID.randomUUID().getMostSignificantBits() + "L");
             return value;
         }
 
         @Override
         public void apply(TopLevelClass compilationUnit) {
-            compilationUnit.addField(apply());
+            Field f = apply();
+            compilationUnit.addField(f);
         }
     },
 
-    PRIMARY_KEY(new Field("id", new FullyQualifiedJavaType(Serializable.class.getName()))) {
+    PRIMARY_KEY(new Field("id", new FullyQualifiedJavaType("T"))) {
         @Override
         public Field apply() {
             Field value = getValue();
@@ -45,14 +49,6 @@ public enum DefaultJavaField {
         @Override
         public void apply(TopLevelClass compilationUnit) {
             compilationUnit.addField(apply());
-            Method method = getJavaBeansSetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
-            method = getJavaBeansGetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
         }
     },
 
@@ -67,14 +63,6 @@ public enum DefaultJavaField {
         @Override
         public void apply(TopLevelClass compilationUnit) {
             compilationUnit.addField(apply());
-            Method method = getJavaBeansSetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
-            method = getJavaBeansGetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
         }
 
     },
@@ -90,18 +78,10 @@ public enum DefaultJavaField {
         @Override
         public void apply(TopLevelClass compilationUnit) {
             compilationUnit.addField(apply());
-            Method method = getJavaBeansSetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
-            method = getJavaBeansGetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
         }
     },
 
-    CREATED_BY(new Field("createdBy", new FullyQualifiedJavaType(Serializable.class.getName()))) {
+    CREATED_BY(new Field("createdBy", new FullyQualifiedJavaType("T"))) {
         @Override
         Field apply() {
             Field value = getValue();
@@ -112,18 +92,10 @@ public enum DefaultJavaField {
         @Override
         public void apply(TopLevelClass compilationUnit) {
             compilationUnit.addField(apply());
-            Method method = getJavaBeansSetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
-            method = getJavaBeansGetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
         }
     },
 
-    LAST_MODIFIED_BY(new Field("lastModifiedBy", new FullyQualifiedJavaType(Serializable.class.getName()))) {
+    LAST_MODIFIED_BY(new Field("lastModifiedBy", new FullyQualifiedJavaType("T"))) {
         @Override
         Field apply() {
             Field value = getValue();
@@ -134,14 +106,6 @@ public enum DefaultJavaField {
         @Override
         public void apply(TopLevelClass compilationUnit) {
             compilationUnit.addField(apply());
-            Method method = getJavaBeansSetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
-            method = getJavaBeansGetter();
-            if (Objects.nonNull(method)) {
-                compilationUnit.addMethod(method);
-            }
         }
     };
 
@@ -159,12 +123,23 @@ public enum DefaultJavaField {
         return field;
     }
 
+    @Override
+    public String toString() {
+        return field.getName();
+    }
+
+    public void bean(final TopLevelClass compilationUnit) {
+        checkNotNull(compilationUnit);
+        compilationUnit.addMethod(getJavaBeansSetter());
+        compilationUnit.addMethod(getJavaBeansGetter());
+    }
+
     public ImmutablePair<Method, Method> getJavaBeanMethod() {
         return ImmutablePair.of(getJavaBeansSetter(), getJavaBeansGetter());
     }
 
     Method getJavaBeansGetter() {
-        Method method = new Method(getSetterMethodName(getValue().getName()));
+        Method method = new Method(getGetterMethodName(getValue().getName(), getValue().getType()));
         method.setVisibility(PUBLIC);
         method.setReturnType(getValue().getType());
         method.addBodyLine("return " + getValue().getName() + ";");
@@ -179,4 +154,9 @@ public enum DefaultJavaField {
         method.addBodyLine("this." + getValue().getName() + " = " + "checkNotNull(" + getValue().getName() + ")" + ";");
         return method;
     }
+
+    public static List<String> getName() {
+        return Arrays.stream(values()).map(DefaultJavaField::toString).toList();
+    }
+
 }
